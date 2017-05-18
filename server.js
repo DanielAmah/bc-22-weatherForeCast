9dbecac5140b743e495a7f0ab9b652bb54757688
Andela <amahdanieljack@gmail.com>
2017-05-18 21:23:47 +0100

    remove routes from server.js

--- server.js
+++ server.js
@@ -1,37 +1,6 @@
 var express = require('express');
 var app = express();
 
-var firebase = require("firebase");
-var BodyParser = require("body-parser");
-
-var config = {
-        apiKey: "AIzaSyA8nkxbEk7by1ZR9jzMSvW5cVFwgwYMt6c",
-        authDomain: "bc-22-weatherforecastapp.firebaseapp.com",
-        databaseURL: "https://bc-22-weatherforecastapp.firebaseio.com",
-        projectId: "bc-22-weatherforecastapp",
-        storageBucket: "bc-22-weatherforecastapp.appspot.com",
-        messagingSenderId: "994611442270"
-    };
-
-    firebase.initializeApp(config);
-
-app.use(BodyParser.json());
-
-app.use(BodyParser.urlencoded({extended:true}));
-
-app.post('/index',function(req, res){
-
-var email = req.body.email ;
-var pass = req.body.password;
-const auth = firebase.auth();
-const promise = auth.signInWithEmailAndPassword(email, pass);
-promise.catch(e=>console.log(e.message));
-
-});
-
-
-
-
 
 
 
